3be0363f901383cf06cfad4257f96b8f48ced25c
Bill Wang <bill.wang>
2019-07-07 19:53:17 +1000

    upgrade to terraform 0.12

--- variables.tf
+++ variables.tf
@@ -108,32 +108,32 @@ variable "ebs_optimized" {
 
 variable "block_device_mappings" {
   description = "Specify volumes to attach to the instance besides the volumes specified by the AMI"
-  type        = list(string)
-  default     = []
+  type        = map(string)
+  default     = {}
 }
 
 variable "instance_market_options" {
   description = "The market (purchasing) option for the instances"
-  type        = list(string)
-  default     = []
+  type        = map(string)
+  default     = {}
 }
 
 variable "placement" {
   description = "The placement specifications of the instances"
-  type        = list(string)
-  default     = []
+  type        = map(string)
+  default     = {}
 }
 
 variable "credit_specification" {
   description = "Customize the credit specification of the instances"
-  type        = list(string)
-  default     = []
+  type        = map(string)
+  default     = {}
 }
 
 variable "elastic_gpu_specifications" {
   description = "Specifications of Elastic GPU to attach to the instances"
-  type        = list(string)
-  default     = []
+  type        = map(string)
+  default     = {}
 }
 
 variable "disable_api_termination" {
